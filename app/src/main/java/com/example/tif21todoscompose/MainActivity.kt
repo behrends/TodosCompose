19904df53dea7949f0d0ada5b7d2757cac4bda40
Erik Behrends <behrends@users.noreply.github.com>
2023-10-25 16:46:52 +0200

    Minimales Material 3 TextField

--- app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
+++ app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -38,5 +39,9 @@ fun TodosApp() {
 
 @Composable
 fun TodoInput() {
-    Text(stringResource(R.string.enter_todo))
+    TextField(
+        value = "",
+        onValueChange = {},
+        label = { Text(stringResource(R.string.enter_todo)) },
+    )
 }
