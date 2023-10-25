8ca335a85fdb6a91964e2da1492b9a3d735df574
Erik Behrends <behrends@users.noreply.github.com>
2023-10-25 19:26:41 +0200

    Todo-Liste mit LazyColumn

--- app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
+++ app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.tif21todoscompose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -19,7 +22,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,8 +52,18 @@ fun TodosApp() {
 
 @Composable
 fun TodoInput() {
+    var todos by remember { mutableStateOf(listOf("Sport", "Einkaufen", "Kotlin lernen")) }
+    Column {
+        TodoInput(onSave = {todos = todos + it})
+        LazyColumn(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+            items(todos) { TodoItem(it) }
+        }
+    }
+}
+
+@Composable
+fun TodoInput(onSave: (String) -> Unit) {
     var todo by remember { mutableStateOf("") }
-    val context = LocalContext.current
     Row {
         TextField(
             value = todo,
@@ -59,10 +71,13 @@ fun TodoInput() {
             label = { Text(stringResource(R.string.enter_todo)) },
         )
         Spacer(modifier = Modifier.width(16.dp))
-        Button(onClick = {
-            Toast.makeText(context, todo, Toast.LENGTH_SHORT).show()
-        }) {
+        Button(onClick = {onSave(todo)})  {
             Text(stringResource(R.string.save))
         }
     }
+}
+
+@Composable
+fun TodoItem(todo: String) {
+    Text(text = todo, modifier = Modifier.padding(8.dp))
 }
