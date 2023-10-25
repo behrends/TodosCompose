4e0a6e99240c986206f514a264e098edab43b0b7
Erik Behrends <behrends@users.noreply.github.com>
2023-10-25 17:12:20 +0200

    Etwas Abstand zwischen TextField und Button mit Spacer

--- app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
+++ app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -14,6 +18,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.tif21todoscompose.ui.theme.TIF21TodosComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -47,6 +52,7 @@ fun TodoInput() {
             onValueChange = { /*TODO*/ },
             label = { Text(stringResource(R.string.enter_todo)) },
         )
+        Spacer(modifier = Modifier.width(16.dp))
         Button(onClick = { /*TODO*/ }) {
             Text(stringResource(R.string.save))
         }
