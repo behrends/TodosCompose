d78da49f30dcc0ecbb79271b9cca076863991da0
Erik Behrends <behrends@users.noreply.github.com>
2023-10-25 17:51:53 +0200

    State im Composable

--- app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
+++ app/src/main/java/com/example/tif21todoscompose/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.tif21todoscompose
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -15,7 +14,12 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -46,14 +50,18 @@ fun TodosApp() {
 
 @Composable
 fun TodoInput() {
+    var todo = remember { mutableStateOf("") }
+    val context = LocalContext.current
     Row {
         TextField(
-            value = "",
-            onValueChange = { /*TODO*/ },
+            value = todo.value,
+            onValueChange = { todo.value = it },
             label = { Text(stringResource(R.string.enter_todo)) },
         )
         Spacer(modifier = Modifier.width(16.dp))
-        Button(onClick = { /*TODO*/ }) {
+        Button(onClick = {
+            Toast.makeText(context, todo.value, Toast.LENGTH_SHORT).show()
+        }) {
             Text(stringResource(R.string.save))
         }
     }
